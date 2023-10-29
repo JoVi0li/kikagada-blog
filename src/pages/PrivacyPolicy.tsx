ff5ce6496de5aae1f3173968c713bd877faa06b9
Jovioli <jovioli.dev04@gmail.com>
2023-10-29 15:59:01 -0300

    feat: add privacy policy page

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,7 +1,110 @@
+import '../styles/PrivacyPolicyStyle.css'
 
 function PrivacyPolicy() {
     return <>
-        Privacy Policy
+        <header>
+            <div>
+                <h1>Política de Privacidade - Kikagada</h1>
+            </div>
+            <text>Última atualização: 29/10/2023</text>
+        </header>
+        <main>
+        <h4>Kikagada ("nós", "nossos" ou "nosso") está comprometido em proteger a privacidade de seus usuários. Esta Política de Privacidade descreve como coletamos, usamos, compartilhamos e protegemos suas informações pessoais quando você utiliza o aplicativo móvel Kikagada ("Aplicativo") e seus serviços associados. Ao utilizar o Aplicativo, você concorda com as práticas descritas nesta Política de Privacidade.</h4>
+
+        <h2>Informações que Coletamos</h2>
+
+        <ol>
+        <li>
+            <h3>Informações Fornecidas pelo Usuário:</h3>
+            <p>Ao criar uma conta no Kikagada, podemos coletar certas informações pessoais, como seu nome, endereço de e-mail e foto de perfil.</p>
+        </li>
+
+        <li>
+            <h3>Avaliações e Conteúdo:</h3>
+            <p>Coletamos as avaliações e o conteúdo que você cria e compartilha no Aplicativo, incluindo avaliações de banheiros e qualquer texto, fotos ou outras mídias associadas.</p>
+        </li>
+
+        <li>
+            <h3>Informações de Uso:</h3>
+            <p>Coletamos informações sobre como você utiliza o Aplicativo, incluindo informações do dispositivo, dados de registro e análises. Isso pode incluir seu endereço IP, tipo de dispositivo, sistema operacional e as páginas ou recursos com os quais você interage.</p>
+        </li>
+        </ol>
+
+        <h2>Como Utilizamos Suas Informações</h2>
+
+        <p>Utilizamos as informações que coletamos para os seguintes fins:</p>
+
+        <ol>
+        <li>
+            <h3>Conta de Usuário:</h3>
+            <p>Para criar e gerenciar sua conta de usuário.</p>
+        </li>
+
+        <li>
+            <h3>Avaliações:</h3>
+            <p>Para exibir suas avaliações e conteúdo relacionado a outros usuários do Aplicativo.</p>
+        </li>
+
+        <li>
+            <h3>Comunicação:</h3>
+            <p>Para comunicar com você sobre sua conta, atualizações, promoções e outras informações relacionadas ao Aplicativo.</p>
+        </li>
+
+        <li>
+            <h3>Melhorias:</h3>
+            <p>Para aprimorar a funcionalidade, conteúdo e experiência do usuário no Aplicativo.</p>
+        </li>
+
+        <li>
+            <h3>Análises:</h3>
+            <p>Para analisar padrões de uso e tendências a fim de aprimorar o desempenho do Aplicativo.</p>
+        </li>
+        </ol>
+
+        <h2>Compartilhamento de Informações</h2>
+
+        <p>Não compartilhamos suas informações pessoais com terceiros, exceto nos seguintes casos:</p>
+
+        <ol>
+        <li>
+            <h3>Com o Seu Consentimento:</h3>
+            <p>Podemos compartilhar suas informações com terceiros se você nos der consentimento para fazê-lo.</p>
+        </li>
+
+        <li>
+            <h3>Prestadores de Serviços:</h3>
+            <p>Podemos compartilhar suas informações com prestadores de serviços de terceiros que nos auxiliam na prestação e melhoria de nossos serviços, incluindo o Firebase para armazenamento e gerenciamento de dados.</p>
+        </li>
+
+        <li>
+            <h3>Conformidade Legal:</h3>
+            <p>Podemos divulgar suas informações se exigido por lei, regulamento ou em resposta a solicitações legais.</p>
+        </li>
+        </ol>
+
+        <h2>Segurança dos Dados</h2>
+
+        <p>Tomamos medidas razoáveis para proteger suas informações pessoais, mas nenhum método de transmissão pela internet ou armazenamento eletrônico é completamente seguro. Não podemos garantir a segurança absoluta de seus dados.</p>
+
+        <h2>Suas Escolhas</h2>
+
+        <p>Você pode controlar e gerenciar suas informações pessoais ajustando as configurações de sua conta dentro do Aplicativo. Você também pode excluir sua conta a qualquer momento.</p>
+
+        <h2>Alterações a esta Política de Privacidade</h2>
+
+        <p>Podemos atualizar esta Política de Privacidade para refletir mudanças em nossas práticas ou por outros motivos operacionais, legais ou regulatórios. A política revisada entrará em vigor quando postada no Aplicativo.</p>
+
+        </main>
+
+        <footer>
+
+        <h2>Entre em Contato</h2>
+
+        <p>Se você tiver alguma dúvida ou preocupação sobre esta Política de Privacidade, entre em contato conosco pelo e-mail jovioli.dev04@gmail.com.</p>
+
+        <p>Ao utilizar o aplicativo Kikagada, você concorda com os termos e condições descritos nesta Política de Privacidade.</p>
+
+        </footer>
     </>
 }
 
